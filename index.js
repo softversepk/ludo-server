646037bee2255751ecdd0a277b31bf2ce0312c04
softversepk <softverse.pk@gmail.com>
2026-05-29 14:28:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1347,7 +1347,7 @@ Object.defineProperty(rooms, 'scheduleRoomDelete', {
 
 // Initialize Game Chat Server
 const GameChatServer = require("./gameChatServer");
-const gameChatServer = new GameChatServer(io);
+const gameChatServer = new GameChatServer(io, admin, rooms, userSockets);
 gameChatServer.initialize();
 
 console.log("✅ Game Chat Server initialized with secure Socket.IO");
